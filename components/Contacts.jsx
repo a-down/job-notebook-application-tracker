e4cc2a26fb340a9883bed2cecb0f277af899ca4c
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-03 12:58:01 -0500

    contact card feature

--- components/Contacts.jsx
+++ components/Contacts.jsx
@@ -1,12 +1,11 @@
-import { SiLinkedin } from 'react-icons/si'
-import { PiLinkedinLogo, PiLinkedinLogoFill, PiPhone, PiEnvelope } from 'react-icons/pi'
+import { PiLinkedinLogoBold, PiPhoneBold, PiEnvelopeBold } from 'react-icons/pi'
 
 
 export default function Contacts({ sharedStyle, contacts }) {
   console.log(contacts)
   return (
     <div className={`${sharedStyle} col-span-3 mb-1 h-full grid grid-cols-2 gap-x-1 gap-y-2`}>
-      <h6 className="text-lg font-regular col-span-2 ">Contacts</h6>
+      <h6 className="text-lg font-regular col-span-2 mb-1">Contacts</h6>
 
       {contacts && (
         contacts.map(contact => (
@@ -16,22 +15,22 @@ export default function Contacts({ sharedStyle, contacts }) {
 
             {contact.contact_linkedin && (
               <a href={contact.contact_linkedin} target='_blank' className='flex gap-1 items-center pl-0.5 mb-1 text-gray-7 hover:text-brand-primary'>
-                <PiLinkedinLogo className='text-brand-primary text-xs col-span-1'/>
-                <p className='text-xxs truncate'>{contact.contact_linkedin.split('.com')[1]}</p>
+                <PiLinkedinLogoBold className='text-brand-primary text-xs'/>
+                <p className='text-xxs overflow-x-clip' style={{textOverflow: 'ellipsis'}}>{contact.contact_linkedin.split('.com')[1]}</p>
               </a>
             )}
 
             {contact.contact_email && (
               <a href={`mailto: ${contact.contact_email}`} className='flex gap-1 items-center pl-0.5 mb-1 text-gray-7 hover:text-brand-primary'>
-                <PiEnvelope className='text-brand-primary text-xs col-span-1'/>
-               <p className='text-xxs truncate'>{contact.contact_email}fhasodfjoiadjsgiasjgoi</p>
+                <PiEnvelopeBold className='text-brand-primary text-xs flex-shrink-0'/>
+               <p className='text-xxs overflow-x-clip' style={{textOverflow: 'ellipsis'}}>{contact.contact_email}fhasodfjoiadjsgiasjgoi</p>
               </a>
             )}
 
             {contact.contact_phone && (
               <a href={`tel: ${contact.contact_phone}`} className='flex gap-1 items-center pl-0.5 mb-1 text-gray-7 hover:text-brand-primary'>
-                <PiPhone className='text-brand-primary text-xs col-span-1'/>
-                <p className='text-xxs truncate'>{contact.contact_phone}</p>
+                <PiPhoneBold className='text-brand-primary text-xs'/>
+                <p className='text-xxs overflow-x-clip' style={{textOverflow: 'ellipsis'}}>{contact.contact_phone}</p>
               </a>
             )}
           </div>
--- components/ToDoItem.jsx
+++ components/ToDoItem.jsx
@@ -1,5 +1,4 @@
 "use client"
-import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import { PiCircle, PiCheckCircle, PiCheckCircleFill, PiCheckCircleDuotone } from 'react-icons/pi'
 
