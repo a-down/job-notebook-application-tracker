d3f3853b1a7420e2de389503d6b943b6f8cf42fd
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-05 08:47:51 -0500

    title hover behavior

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center bg-white h-24 w-full px-16 py-6 drop-shadow-brand">
       <Link href="/" className=''>
-        <h1 className=" font-display font-semibold text-4xl hover:text-brand-primary">MyJobs</h1>
+        <h1 className=" font-display font-semibold text-4xl hover:text-brand-primary duration-300">MyJobs</h1>
       </Link>
       
       <div className='flex gap-4 font-body'>
