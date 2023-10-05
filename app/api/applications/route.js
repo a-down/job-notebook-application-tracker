d94faf0c7a784f17f4b51ab354f5f0f9830f2a6b
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-05 10:11:51 -0500

    routes update

--- app/api/applications/route.js
+++ app/api/applications/route.js
@@ -3,7 +3,7 @@ import Application from "@/models/Application";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-
+  const body = await req.json()
   await connectMongoDB();
   await Application.create(req.body)
   return NextResponse.json({message: "Application Created", status: 201})
--- app/api/users/route.js
+++ app/api/users/route.js
@@ -4,7 +4,6 @@ import { NextResponse } from "next/server";
 
 export async function POST(req) {
   const body = await req.json()
-  console.log(body)
   await connectMongoDB();
   await User.create(body)
   return NextResponse.json({message: "User Created", status: 201})
