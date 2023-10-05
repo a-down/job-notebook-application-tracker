a213194f4db9c053da9f2a564ec8c818cfe84562
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-05 15:17:32 -0500

    ClerkProvider

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -34,7 +34,7 @@ export default function Dashboard() {
 
 
   return (
-    // <ClerkProvider>
+    <ClerkProvider>
       <main className="bg-gray-1 min-h-[calc(100vh-96px)] px-16 py-16">
         <h2 className="font-display font-semibold text-4xl mb-12">Dashboard</h2>
 
@@ -85,6 +85,6 @@ export default function Dashboard() {
         
 
       </main>
-    // </ClerkProvider>
+    </ClerkProvider>
   )
 }
