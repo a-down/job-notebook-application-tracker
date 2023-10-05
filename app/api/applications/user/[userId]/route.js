882b5f496db0e8cf72054792058407f78c67f965
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-05 16:03:41 -0500

    application by user

---
+++ app/api/applications/user/[userId]/route.js
@@ -0,0 +1,18 @@
+import connectMongoDB from "@/libs/mongodb";
+import Application from "@/models/Application";
+import { NextResponse } from "next/server";
+
+// export async function POST (req) {
+//   const body = await req.json()
+//   console.log(body)
+//   await connectMongoDB();
+//   await Application.create(body)
+//   return NextResponse.json({message: "Application Created", status: 201})
+// }
+
+export async function GET (req, { params }) {
+  const userId = await params.userId
+  await connectMongoDB();
+  const applications = await Application.find({ user_id: userId })
+  return NextResponse.json(applications)
+}
