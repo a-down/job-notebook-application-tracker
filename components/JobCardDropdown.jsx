0d2c9c6fa546f1ed3696f3102a1d27413a1c4bcb
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-05 16:27:30 -0500

    keys bug

--- components/JobCardDropdown.jsx
+++ components/JobCardDropdown.jsx
@@ -3,14 +3,18 @@ import { Contacts, Files, Notes, ToDo } from '@/components'
 
 export default function JobCardDropdown({ application }) {
 
+  function randomString() {
+    return Math.random() .toString(36) .slice (2)
+  } 
+
   const sharedStyle = 'bg-white p-4 rounded-md drop-shadow-brand'
 
   return (
     <div className=" px-4 pt-[14px] pb-5 min-h-20 w-full rounded-b-md grid grid-cols-5 auto-rows-min gap-4 dropdown">
-      <ToDo sharedStyle={sharedStyle} toDo={application.to_do}/>
-      <Contacts sharedStyle={sharedStyle} contacts={application.contacts}/>
-      <Files sharedStyle={sharedStyle} files={application.files}/>
-      <Notes sharedStyle={sharedStyle} notes={application.notes}/>
+      <ToDo sharedStyle={sharedStyle} toDo={application.to_do} key={randomString}/>
+      <Contacts sharedStyle={sharedStyle} contacts={application.contacts} key={randomString}/>
+      <Files sharedStyle={sharedStyle} files={application.files} key={randomString}/>
+      <Notes sharedStyle={sharedStyle} notes={application.notes} key={randomString}/>
     </div>
   )
 }
