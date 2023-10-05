4f44276424a888c20353ff6f8f22b516b7503d6a
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-05 14:26:50 -0500

    clerk start

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-
+import { ClerkProvider } from '@clerk/nextjs'
 import JobCard from '@/components/JobCard'
 import { useEffect, useState } from 'react'
 import applications from '@/mockProjects'
@@ -32,60 +32,57 @@ export default function Dashboard() {
 
 
   return (
-    <main className="bg-gray-1 min-h-[calc(100vh-96px)] px-16 py-16">
-      <h2 className="font-display font-semibold text-4xl mb-12">Dashboard</h2>
+    <ClerkProvider>
+      <main className="bg-gray-1 min-h-[calc(100vh-96px)] px-16 py-16">
+        <h2 className="font-display font-semibold text-4xl mb-12">Dashboard</h2>
 
-      <div className="grid grid-cols-12 gap-4">
+        <div className="grid grid-cols-12 gap-4">
 
-        <div className="grid-cols-2 grid grid-flow-row auto-rows-min gap-4 col-span-10">
+          <div className="grid-cols-2 grid grid-flow-row auto-rows-min gap-4 col-span-10">
 
-          {applications && (
-            applications.map((application) => (
-              <JobCard application={application} key={application._id}/>
-            ))
-          )}
- 
+            {applications && (
+              applications.map((application) => (
+                <JobCard application={application} key={application._id}/>
+              ))
+            )}
+  
+          </div>
+
+          <aside className="col-span-2 bg-gray-3 rounded drop-shadow-brand h-full p-4">
+            <div className='mb-4'>
+              <h6 className='text-lg text-gray-7 font-regular mb-2'>
+                Current
+                <span className='font-thin text-gray-8'> (7)</span>
+              </h6>
+
+              <button className=' text-white bg-brand-primary w-full px-4 py-2 rounded-md border border-brand-primary hover:bg-gray-7 hover:border-gray-7 hover:text-white duration-300 active:border-black mb-1'>Job Title</button>
+              <button className=' text-white bg-brand-primary w-full px-4 py-2 rounded-md border border-brand-primary hover:bg-gray-7 hover:border-gray-7 hover:text-white duration-300 active:border-black mb-1'>Job Title</button>
+              <button className=' text-white bg-brand-primary w-full px-4 py-2 rounded-md border border-brand-primary hover:bg-gray-7 hover:border-gray-7 hover:text-white duration-300 active:border-black mb-1'>Job Title</button>
+              <button className=' text-white bg-brand-primary w-full px-4 py-2 rounded-md border border-brand-primary hover:bg-gray-7 hover:border-gray-7 hover:text-white duration-300 active:border-black mb-1'>Job Title</button>
+              <button className=' text-white bg-brand-primary w-full px-4 py-2 rounded-md border border-brand-primary hover:bg-gray-7 hover:border-gray-7 hover:text-white duration-300 active:border-black mb-1'>Job Title</button>
+              <button className=' text-white bg-brand-primary w-full px-4 py-2 rounded-md border border-brand-primary hover:bg-gray-7 hover:border-gray-7 hover:text-white duration-300 active:border-black mb-1'>Job Title</button>
+              <button className=' text-white bg-brand-primary w-full px-4 py-2 rounded-md border border-brand-primary hover:bg-gray-7 hover:border-gray-7 hover:text-white duration-300 active:border-black mb-1'>Job Title</button>
+
+
+            </div>
+            <div className='mb-4'>
+              <h6 className='text-lg text-gray-7 font-regular mb-2'>
+                Completed
+                <span className='font-thin text-gray-8'> (3)</span>
+              </h6>
+
+              <button className=' text-gray-7 w-full px-4 py-2 rounded-md  border border-gray-7 hover:bg-brand-primary hover:border-brand-primary hover:text-white duration-300 active:border-gray-9 mb-1'>Job Title</button>
+              <button className=' text-gray-7 w-full px-4 py-2 rounded-md  border border-gray-7 hover:bg-brand-primary hover:border-brand-primary hover:text-white duration-300 active:border-gray-9 mb-1'>Job Title</button>
+              <button className=' text-gray-7 w-full px-4 py-2 rounded-md  border border-gray-7 hover:bg-brand-primary hover:border-brand-primary hover:text-white duration-300 active:border-gray-9 mb-1'>Job Title</button>
+            </div>
+            
+          </aside>
+
         </div>
 
-        <aside className="col-span-2 bg-gray-3 rounded drop-shadow-brand h-full p-4">
-          <div className='mb-4'>
-            <h6 className='text-lg text-gray-7 font-regular mb-2'>
-              Current
-              <span className='font-thin text-gray-8'> (7)</span>
-            </h6>
+        
 
-            <button className=' text-white bg-brand-primary w-full px-4 py-2 rounded-md border border-brand-primary hover:bg-gray-7 hover:border-gray-7 hover:text-white duration-300 active:border-black mb-1'>Job Title</button>
-            <button className=' text-white bg-brand-primary w-full px-4 py-2 rounded-md border border-brand-primary hover:bg-gray-7 hover:border-gray-7 hover:text-white duration-300 active:border-black mb-1'>Job Title</button>
-            <button className=' text-white bg-brand-primary w-full px-4 py-2 rounded-md border border-brand-primary hover:bg-gray-7 hover:border-gray-7 hover:text-white duration-300 active:border-black mb-1'>Job Title</button>
-            <button className=' text-white bg-brand-primary w-full px-4 py-2 rounded-md border border-brand-primary hover:bg-gray-7 hover:border-gray-7 hover:text-white duration-300 active:border-black mb-1'>Job Title</button>
-            <button className=' text-white bg-brand-primary w-full px-4 py-2 rounded-md border border-brand-primary hover:bg-gray-7 hover:border-gray-7 hover:text-white duration-300 active:border-black mb-1'>Job Title</button>
-            <button className=' text-white bg-brand-primary w-full px-4 py-2 rounded-md border border-brand-primary hover:bg-gray-7 hover:border-gray-7 hover:text-white duration-300 active:border-black mb-1'>Job Title</button>
-            <button className=' text-white bg-brand-primary w-full px-4 py-2 rounded-md border border-brand-primary hover:bg-gray-7 hover:border-gray-7 hover:text-white duration-300 active:border-black mb-1'>Job Title</button>
-
-
-          </div>
-          <div className='mb-4'>
-            <h6 className='text-lg text-gray-7 font-regular mb-2'>
-              Completed
-              <span className='font-thin text-gray-8'> (3)</span>
-            </h6>
-
-            <button className=' text-gray-7 w-full px-4 py-2 rounded-md  border border-gray-7 hover:bg-brand-primary hover:border-brand-primary hover:text-white duration-300 active:border-gray-9 mb-1'>Job Title</button>
-            <button className=' text-gray-7 w-full px-4 py-2 rounded-md  border border-gray-7 hover:bg-brand-primary hover:border-brand-primary hover:text-white duration-300 active:border-gray-9 mb-1'>Job Title</button>
-            <button className=' text-gray-7 w-full px-4 py-2 rounded-md  border border-gray-7 hover:bg-brand-primary hover:border-brand-primary hover:text-white duration-300 active:border-gray-9 mb-1'>Job Title</button>
-
-          </div>
-          
-
-          
-
-          
-        </aside>
-
-      </div>
-
-      
-
-    </main>
+      </main>
+    </ClerkProvider>
   )
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,20 +1,23 @@
 import Link from 'next/link'
+import { ClerkProvider } from '@clerk/nextjs'
 
 
 export default function Header() {
 
   return (
-    <header className="flex justify-between items-center bg-white h-24 w-full px-16 py-6 drop-shadow-brand">
-      <Link href="/" className=''>
-        <h1 className=" font-display font-semibold text-4xl hover:text-brand-primary duration-300">MyJobs</h1>
-      </Link>
-      
-      <nav className='flex gap-4 font-body'>
-        <Link href="/about" className='hover:text-brand-primary' style={{transitionDuration: '.3s'}}>About</Link>
-        <Link href="/howitworks" className='hover:text-brand-primary' style={{transitionDuration: '.3s'}}>How It Works</Link>
-        <Link href="/dashboard" className='hover:text-brand-primary' style={{transitionDuration: '.3s'}}>Dashboard</Link>
-        <Link href="/account" className='hover:text-brand-primary' style={{transitionDuration: '.3s'}}>Account</Link>
-      </nav>
-    </header>
+    <ClerkProvider>
+      <header className="flex justify-between items-center bg-white h-24 w-full px-16 py-6 drop-shadow-brand">
+        <Link href="/" className=''>
+          <h1 className=" font-display font-semibold text-4xl hover:text-brand-primary duration-300">MyJobs</h1>
+        </Link>
+        
+        <nav className='flex gap-4 font-body'>
+          <Link href="/about" className='hover:text-brand-primary' style={{transitionDuration: '.3s'}}>About</Link>
+          <Link href="/howitworks" className='hover:text-brand-primary' style={{transitionDuration: '.3s'}}>How It Works</Link>
+          <Link href="/dashboard" className='hover:text-brand-primary' style={{transitionDuration: '.3s'}}>Dashboard</Link>
+          <Link href="/account" className='hover:text-brand-primary' style={{transitionDuration: '.3s'}}>Account</Link>
+        </nav>
+      </header>
+    </ClerkProvider>
   )
 }
