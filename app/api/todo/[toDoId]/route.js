7866daa2985f95336b131aeaf2102de96c075a14
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-10 09:31:44 -0500

    put todo

---
+++ app/api/todo/[toDoId]/route.js
@@ -0,0 +1,33 @@
+import connectMongoDB from "@/libs/mongodb";
+import ToDo from "@/models/ToDo";
+import { NextResponse } from "next/server";
+
+
+export async function GET (req, { params }) {
+  await connectMongoDB();
+  const toDo = await ToDo.findById(params.toDoId)
+  return NextResponse.json(toDo)
+}
+
+export async function PUT (req, { params }) {
+  const body = await req.json()
+  await connectMongoDB();
+  const toDo = await ToDo.findByIdAndUpdate(params.toDoId, body, {new: true})
+  return NextResponse.json(toDo)
+}
+
+// export async function POST (req) {
+//   const {to_do, ...body} = await req.json()
+//   await connectMongoDB();
+//   const application = await Application.create(body)
+//   let toDoIdArr = []
+//   for (const item of to_do) {
+//     const res = await ToDo.create(item)
+//     await Application.findByIdAndUpdate(application._id, {
+//       $push: { to_do: res._id }
+//     }, { new: true })
+//   }
+
+//   // await Application.create(body)
+//   return NextResponse.json({message: "Application Created", status: 201})
+// }
