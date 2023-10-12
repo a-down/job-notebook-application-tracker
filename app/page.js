829be35ccfff60d700abf568a2e052c50b4d1f64
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-12 18:21:15 -0500

    inline svg and features formatting

--- app/page.js
+++ app/page.js
@@ -1,11 +1,65 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { Header } from '@/components'
+import primaryWave from '@/public/primary-wave.svg'
 
 export default function Home() {
   return (
-    <main className="bg-brand-dark min-h-screen">
-      <Header isDark={true} />
+    <>
+      <div className='bg-brand-dark'>
+        <Header isDark={true} />
 
-    </main>
+        <main className="bg-brand-dark min-h-screen overflow-clip">
+        
+          {/* hero section */}
+          <div className='grid grid-cols-12 gap-4 pt-24 h-[636px] px-16'>
+            <div className='col-span-4'>
+              <div className='bg-white h-[324px] mb-6'>
+
+              </div>
+              <button className='h-20 bg-brand-primary text-brand-dark rounded-full w-full text-lg font-bold'>Start your notebook</button>
+            </div>
+
+            <div className="col-span-7 col-start-6">
+              <div className='w-full flex justify-end mb-12'>
+                <Link href='/howitworks' className='py-3 px-4 text-brand-primary font-bold border-2 border-brand-primary rounded-full hover:bg-brand-primary hover:text-brand-dark' style={{transitionDuration: '.3s'}}>
+                  Try it out
+                </Link>
+              </div>
+
+              <div className="bg-white rounded-md w-[904px] h-[638px] overflow z-0">
+
+              </div>
+            </div>
+          </div>
+
+          <div className=" w-screen h-[576px] relative">
+
+            <svg width="100%" height="576" viewBox="0 0 1444 576" preserveAspectRatio="none" fill="none" xmlns="http://www.w3.org/2000/svg"
+              className='absolute top-0 left-0 z-0'>
+              <path d="M0 576V0C866.393 26.8364 1153.26 47.687 1440 98.8432V576H0Z" fill="#9DD4C7"/>
+            </svg>
+
+            <div className='relative z-10'>
+              <h1 className='text-white'>How it works</h1>
+            </div>
+
+
+
+          </div>
+
+          <div className='w-full h-[664px]'>
+
+          </div>
+
+        </main>
+
+        <footer className="w-full h-[260px] bg-[url('../public/footer-wave.svg')] bg-left-top z-10 relative">
+          
+
+        </footer>
+      </div>
+    </>
+    
   )
 }
