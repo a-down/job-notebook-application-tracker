d8994536f3e3799fad8fe34c4b226ba4658c48e4
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-13 15:05:17 -0500

    linke bug

--- app/page.js
+++ app/page.js
@@ -142,7 +142,7 @@ export default function Home() {
               <Link href='/' className=' hover:text-brand-primary' style={{transitionDuration: '.3s'}}>Home</Link>
               <Link href='/about' className='hover:text-brand-primary' style={{transitionDuration: '.3s'}}>About</Link>
               <Link href='/howitworks' className='hover:text-brand-primary' style={{transitionDuration: '.3s'}}>How It Works</Link>
-              <Link href='/Dashboard' className='hover:text-brand-primary' style={{transitionDuration: '.3s'}}>Dashboard</Link>
+              <Link href='/dashboard' className='hover:text-brand-primary' style={{transitionDuration: '.3s'}}>Dashboard</Link>
             </div>
           </div>
 
