8c776df7000f5cda7ae7dcddc5525daffd304926
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-15 12:54:11 -0500

    todo input styling

--- components/ToDo.jsx
+++ components/ToDo.jsx
@@ -1,23 +1,34 @@
 "use client"
 import { ToDoItem } from '@/components'
+import { PiPlusBold } from 'react-icons/pi'
 
 
 export default function ToDo({ sharedStyle, toDo, setProgressPercentage, updateCard }) {
   // const [ toDoState, ]
 
   return (
-    <div className={`${sharedStyle} col-span-2 row-span-2 mb-1 h-full`}>
+    <div className={`${sharedStyle} col-span-2 row-span-2 mb-1 h-full flex flex-col`}>
       <h6 className="text-lg font-regular mb-3">
         To Do
       </h6>
 
-      <div className='flex flex-col items-start gap-2'>
+      <div className='flex flex-col items-start gap-2 mb-2'>
         {toDo && (
           toDo.map((item, index) => (
             <ToDoItem item={item} key={index} setProgressPercentage={setProgressPercentage} updateCard={updateCard}/>
           ))
         )}
       </div>
+
+      <div className='flex items-end h-full'>
+        <form className='flex gap-2 items-center justify-start'>
+          <button className=''>
+            <PiPlusBold className='text-brand-primary text-lg rounded-sm'/>
+          </button>
+          <input className='w-full rounded-md border border-brand-primary px-1.5 py-1 text-xs focus:border-brand-primary'/>
+        </form>
+      </div>
+      
     </div>
   )
 }
