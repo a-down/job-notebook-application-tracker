6fb0c3e57c14813aa08f2d5279034cb91c552433
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-17 14:48:03 -0500

    responsive header

--- app/page.js
+++ app/page.js
@@ -45,7 +45,7 @@ export default function Home() {
         <main className="bg-brand-dark min-h-screen">
         
           {/* hero section */}
-          <div className='grid grid-cols-12 gap-4 pt-24 h-[636px] px-16 2xl:px-32'>
+          <div className='grid grid-cols-12 gap-4 pt-24 h-[636px] px-4 md:px-16 2xl:px-32'>
             <div className='col-span-4'>
               <div className='mb-16'>
                 <p className='text-xl text-white leading-normal mb-4'>
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,8 @@
+"use client"
 import Link from 'next/link'
 import { UserButton, auth } from '@clerk/nextjs'
+import { BiMenu } from 'react-icons/bi'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 
 export default function Header({ isDark, activePage }) {
   let backroundColor = isDark ? 'bg-brand-dark' : 'bg-white'
@@ -8,12 +11,12 @@ export default function Header({ isDark, activePage }) {
 
   return (
     <>
-      <header className={`${backroundColor} flex justify-between items-center h-24 w-full px-16 2xl:px-32 py-6 drop-shadow-brand`}>
+      <header className={`${backroundColor} flex justify-between items-center h-24 w-full px-4 md:px-16 2xl:px-32 py-6 drop-shadow-brand`}>
         <Link href="/" className=''>
           <h1 className={`${titleColor} font-display font-semibold text-4xl duration-300`}>Job Notebook</h1>
         </Link>
         
-        <nav className='flex gap-4 font-body items-center'>
+        <nav className='hidden md:flex gap-4 font-body items-center md:'>
 
           {activePage === 'about' ? (
             <Link href="/about" className='text-brand-primary hover:text-brand-soft' style={{transitionDuration: '.3s'}}>About</Link>
@@ -34,6 +37,41 @@ export default function Header({ isDark, activePage }) {
           )}
 
           <UserButton afterSignOutUrl='/'/>
+        </nav>
+
+        <nav className='md:hidden'>
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger className='h-full flex justify-center'>
+              <BiMenu className='text-brand-primary text-3xl hover:text-white duration-300' />
+            </DropdownMenu.Trigger>
+
+            <DropdownMenu.Portal>
+              <DropdownMenu.Content>
+                <div className='relative right-4 w-screen mt-4 p-4 pb-8 bg-brand-dark drop-shadow-brand rounded-md text-right flex flex-col items-end gap-6 mobile-nav'>
+                  <UserButton afterSignOutUrl='/'/>
+                  {activePage === 'about' ? (
+                    <Link href="/about" className='text-brand-primary hover:text-brand-soft' style={{transitionDuration: '.3s'}}>About</Link>
+                  ) : (
+                    <Link href="/about" className={linkColor} style={{transitionDuration: '.3s'}}>About</Link>
+                  )}
+
+                  {activePage === 'howitworks' ? (
+                    <Link href="/howitworks" className='text-brand-primary hover:text-brand-soft' style={{transitionDuration: '.3s'}}>How It Works</Link>
+                  ) : (
+                    <Link href="/howitworks" className={linkColor} style={{transitionDuration: '.3s'}}>How It Works</Link>
+                  )}
+
+                  {activePage === 'dashboard' ? (
+                    <Link href="/dashboard" className='text-brand-primary hover:text-brand-soft' style={{transitionDuration: '.3s'}}>Dashboard</Link>
+                  ) : (
+                    <Link href="/dashboard" className={linkColor} style={{transitionDuration: '.3s'}}>Dashboard</Link>
+                  )}
+
+                </div>
+
+              </DropdownMenu.Content>
+            </DropdownMenu.Portal>
+          </DropdownMenu.Root>
         </nav>
       </header>
     </>
