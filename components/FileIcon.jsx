3ec9669ee94917499749d7f4922069da85f27818
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-17 08:58:25 -0500

    delete file icon

--- components/FileIcon.jsx
+++ components/FileIcon.jsx
@@ -1,49 +1,71 @@
+"use client"
 import { SiGoogledocs, SiGoogleslides, SiGooglesheets } from 'react-icons/si'
-import { PiFolderSimpleFill } from 'react-icons/pi'
+import { useState, useEffect } from 'react'
+import { PiFolderSimpleFill, PiTrash, PiTrashFill } from 'react-icons/pi'
 
 export default function FileIcon({ file }) {
+  const [ grayTrashState, setGrayTrashState ] = useState(false)
+  const [ redTrashState, setRedTrashState ] = useState(false)
   let wrapperStyle = 'bg-gray-7 text-2xl rounded-md flex justify-center items-center p-2 duration-300'
+  let icon
 
   switch (file.file_type) {
     case 'Google Doc':
-      return (
-        <a href={file.file_link} target="_blank" className='flex flex-col gap-0.5' >
+      icon = <>
           <div className={`hover:bg-[#2584FC] ${wrapperStyle}`}>
             <SiGoogledocs className='text-white'/>
           </div>
-          <p className='text-xxs text-gray-7 w-10 text-center'>{file.file_name}</p>
-        </a>
-      )
+        </>
       break;
     case 'Google Slide':
-      return (
-        <a href={file.file_link} target="_blank" className='flex flex-col gap-0.5'>
+      icon = <>
           <div className={`hover:bg-[#EEB313] ${wrapperStyle}`}>
             <SiGoogleslides className='text-white'/>
           </div>
-          <p className='text-xxs text-gray-7 w-10 text-center'>{file.file_name}</p>
-        </a>
-      )
+        </>
       break;
     case 'Google Sheet':
-      return (
-        <a href={file.file_link} target="_blank" className='flex flex-col gap-0.5'>
+      icon = <>
           <div className={`hover:bg-[#34A853] ${wrapperStyle}`}>
             <SiGooglesheets className='text-white'/>
           </div>
-          <p className='text-xxs text-gray-7 w-10 text-center'>{file.file_name}</p>
-        </a>
-      )
+      </>
       break;
     case 'Folder':
-      return (
-        <a href={file.file_link} target="_blank">
+      icon = <>
           <div className={`hover:bg-[#5F6367] ${wrapperStyle} text-3xl p-[5px]`}>
             <PiFolderSimpleFill className='text-white'/>
           </div>
-          <p className='text-xxs text-gray-7 w-10 text-center'>{file.file_name}</p>
-        </a>
-      )
+        </>
       break;
   }
+
+  return (
+    <div className='relative' onMouseEnter={() => setGrayTrashState(true)} 
+      onMouseLeave={() => {
+          setGrayTrashState(false) 
+          setRedTrashState(false)
+        }}>
+      <a href={file.file_link} target="_blank" className='flex flex-col gap-0.5' >
+        {icon}
+        <p className='text-xxs text-gray-7 w-10 text-center'>{file.file_name}</p>
+      </a>
+
+      <div className='flex grow justify-end items-center absolute top-[22px] right-0 bg-gray-7 rounded-br-md rounded-tl-md p-px'>
+        {grayTrashState && !redTrashState && (
+          <PiTrash className='text-gray-3 flex-shrink-0 hover:cursor-pointer'
+            onMouseEnter={() => {
+                setGrayTrashState(false)
+                setRedTrashState(true)
+              }} />
+        )}
+        {redTrashState && (
+          <PiTrashFill className='text-red-400 active:text-red-800 flex-shrink-0 hover:cursor-pointer'
+            onMouseLeave={() => setRedTrashState(false)}
+             />
+        )}
+      </div>
+      
+    </div>
+  )
 }
