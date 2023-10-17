890e212fbc767e69c4a823a8d3c8d4692b077207
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-17 13:03:32 -0500

    dashboard loaders

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -9,6 +9,7 @@ export default function Dashboard() {
   const [ completedApplicationsState, setCompletedApplicationsState ] = useState([]) 
   const { isSignedIn, user, isLoaded } = useUser();
   const [ newApplicationModalState, setNewApplicationModalState ] = useState(true)
+  const [ loadingState, setLoadingState ] = useState(true)
   
   useEffect(() => {
     if (user) {
@@ -31,6 +32,7 @@ export default function Dashboard() {
     })
     setCurrentApplicationsState(currentApps)
     setCompletedApplicationsState(completedApps)
+    setLoadingState(false)
   }
 
   return (
@@ -64,9 +66,45 @@ export default function Dashboard() {
                 <JobCard application={application} getApplications={getApplications} isModal={false} key={application._id}/>
               ))
             )}
+
+            {loadingState && (
+              <>
+                <div className='flex justify-between col-span-1 bg-white rounded-md drop-shadow-brand h-[172px] p-4'>
+                  <div className=' h-full animate-pulse flex flex-col gap-1 w-full job-card-upper mb-6'>
+                    <div className='bg-gray-5 rounded-md animate-pulse h-[20px] w-1/2'></div>
+                    <div className='bg-gray-5 rounded-md animate-pulse h-[16px] w-1/4'></div>
+                    <div className='bg-gray-5 rounded-md animate-pulse h-[16px] w-1/6'></div>
+                    <div className='bg-gray-5 rounded-md animate-pulse h-[16px] w-1/4 mt-12'></div>
+                    <div className='bg-gray-5 rounded-md animate-pulse h-[16px] w-2/12'></div>
+                  </div>
+                  <div className='bg-gray-5 rounded-full animate-pulse h-full aspect-square'></div>
+                </div>
+                <div className='flex justify-between col-span-1 bg-white rounded-md drop-shadow-brand h-[172px] p-4'>
+                  <div className=' h-full animate-pulse flex flex-col gap-1 w-full job-card-upper mb-6'>
+                    <div className='bg-gray-5 rounded-md animate-pulse h-[20px] w-1/2'></div>
+                    <div className='bg-gray-5 rounded-md animate-pulse h-[16px] w-1/4'></div>
+                    <div className='bg-gray-5 rounded-md animate-pulse h-[16px] w-1/6'></div>
+                    <div className='bg-gray-5 rounded-md animate-pulse h-[16px] w-1/4 mt-12'></div>
+                    <div className='bg-gray-5 rounded-md animate-pulse h-[16px] w-2/12'></div>
+                  </div>
+                  <div className='bg-gray-5 rounded-full animate-pulse h-full aspect-square'></div>
+                </div>
+                <div className='flex justify-between col-span-1 bg-white rounded-md drop-shadow-brand h-[172px] p-4'>
+                  <div className=' h-full animate-pulse flex flex-col gap-1 w-full job-card-upper mb-6'>
+                    <div className='bg-gray-5 rounded-md animate-pulse h-[20px] w-1/2'></div>
+                    <div className='bg-gray-5 rounded-md animate-pulse h-[16px] w-1/4'></div>
+                    <div className='bg-gray-5 rounded-md animate-pulse h-[16px] w-1/6'></div>
+                    <div className='bg-gray-5 rounded-md animate-pulse h-[16px] w-1/4 mt-12'></div>
+                    <div className='bg-gray-5 rounded-md animate-pulse h-[16px] w-2/12'></div>
+                  </div>
+                  <div className='bg-gray-5 rounded-full animate-pulse h-full aspect-square'></div>
+                </div>
+              </>
+            )}
+
   
           </div>
-
+          
           <aside className="col-span-2 bg-gray-2 rounded drop-shadow-brand h-full min-h-[calc(100vh-308px)] p-4">
             <div className='mb-4'>
               <h6 className='text-lg text-gray-7 font-regular mb-2'>
@@ -80,6 +118,14 @@ export default function Dashboard() {
                 ))
               )}
 
+              {loadingState && (
+                <>
+                  <div className='bg-gray-5 rounded-md h-11 w-full animate-pulse mb-1'></div>
+                  <div className='bg-gray-5 rounded-md h-11 w-full animate-pulse mb-1'></div>
+                  <div className='bg-gray-5 rounded-md h-11 w-full animate-pulse mb-1'></div>
+                </>
+              )}
+
             </div>
             <div className='mb-4'>
               <h6 className='text-lg text-gray-7 font-regular mb-2'>
@@ -91,6 +137,13 @@ export default function Dashboard() {
                 completedApplicationsState.map(application => (
                   <AsideWrapper button={{text: application.role.role_name, style: 'gray-outline'}} key={`${application._id}-modal`} application={application} getApplications={getApplications}/>
                 ))
+              )}
+
+              {loadingState && (
+                <>
+                  <div className='bg-gray-5 rounded-md h-11 w-full animate-pulse mb-1'></div>
+                  <div className='bg-gray-5 rounded-md h-11 w-full animate-pulse mb-1'></div>
+                </>
               )}
 
             </div>
