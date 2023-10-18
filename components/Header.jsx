77d0ea1f205ef3a27385a1bda584d51d52b80d75
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-18 15:35:56 -0500

    header updates

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,13 @@
 "use client"
 import Link from 'next/link'
-import { UserButton } from '@clerk/nextjs'
+import { useState } from 'react'
+import { UserButton, SignedIn, SignedOut, SignInButton } from '@clerk/nextjs'
 import { BiMenu } from 'react-icons/bi'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 
 export default function Header({ isDark, activePage }) {
+  const [ dropdownMenuState, setDropdownMenuState ] = useState(false)
+
   let backgroundColor = isDark ? 'bg-brand-dark' : 'bg-white'
   let titleColor = isDark ? 'text-brand-primary hover:text-white' : 'text-black hover:text-brand-primary'
   let linkColor = isDark ? 'text-white hover:text-brand-primary' : 'text-black hover:text-brand-primary'
@@ -22,13 +25,13 @@ export default function Header({ isDark, activePage }) {
           {activePage === 'home' ? (
             <Link href="/" className='text-brand-primary hover:text-brand-soft' style={{transitionDuration: '.3s'}}>Home</Link>
           ) : (
-            <Link href="/" className={linkColor} style={{transitionDuration: '.3s'}}>About</Link>
+            <Link href="/" className={linkColor} style={{transitionDuration: '.3s'}}>Home</Link>
           )}
 
           {activePage === 'about' ? (
-            <Link href="/about" className='text-brand-primary hover:text-brand-soft' style={{transitionDuration: '.3s'}}>Home</Link>
+            <Link href="/about" className='text-brand-primary hover:text-brand-soft' style={{transitionDuration: '.3s'}}>About</Link>
           ) : (
-            <Link href="/about" className={linkColor} style={{transitionDuration: '.3s'}}>Home</Link>
+            <Link href="/about" className={linkColor} style={{transitionDuration: '.3s'}}>About</Link>
           )}
 
           {/* {activePage === 'tryit' ? (
@@ -43,17 +46,56 @@ export default function Header({ isDark, activePage }) {
             <Link href="/dashboard" className={linkColor} style={{transitionDuration: '.3s'}}>Dashboard</Link>
           )}
 
-          <UserButton afterSignOutUrl='/'/>
+          <SignedIn>
+            <UserButton afterSignOutUrl='/'/>
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton className={`${linkColor} font-body duration-300`}/>
+          </SignedOut>
+          
         </nav>
 
         {/* if small screen, display dropdown hamburger nav menu */}
-        <div className='md:hidden'>
-          <DropdownMenu.Root>
+        <div className='md:hidden '>
+          <BiMenu className={`${linkColor} text-3xl cursor-pointer`} 
+            onClick={() => setDropdownMenuState(prev => !prev)}/>
+
+          {dropdownMenuState && (
+            <div className={`mobile-nav flex items-center justify-end py-8 px-4 md:px-8 lg:px-16 2xl:px-32 justify-right ${backgroundColor} gap-4 drop-shadow-brand rounded-b-md absolute right-0 w-full`}>
+              {activePage === 'home' ? (
+                <Link href="/" className='text-brand-primary hover:text-brand-soft' style={{transitionDuration: '.3s'}}>Home</Link>
+              ) : (
+                <Link href="/" className={linkColor} style={{transitionDuration: '.3s'}}>Home</Link>
+              )}
+
+              {activePage === 'about' ? (
+                <Link href="/about" className='text-brand-primary hover:text-brand-soft' style={{transitionDuration: '.3s'}}>About</Link>
+              ) : (
+                <Link href="/about" className={linkColor} style={{transitionDuration: '.3s'}}>About</Link>
+              )}
+
+              {activePage === 'dashboard' ? (
+                <Link href="/dashboard" className='text-brand-primary hover:text-brand-soft' style={{transitionDuration: '.3s'}}>Dashboard</Link>
+              ) : (
+                <Link href="/dashboard" className={linkColor} style={{transitionDuration: '.3s'}}>Dashboard</Link>
+              )}
+
+              <SignedIn>
+                <UserButton afterSignOutUrl='/'/>
+              </SignedIn>
+
+              <SignedOut>
+                <SignInButton className={`${linkColor} font-body duration-300`}/>
+              </SignedOut>
+            </div>
+          )}
+          {/* <DropdownMenu.Root>
             <DropdownMenu.Trigger className='h-full flex justify-center'>
               <BiMenu className={`${linkColor} text-3xl`} />
             </DropdownMenu.Trigger>
 
-              <DropdownMenu.Content className={`${backgroundColor} ${isDark ? 'px-4 md:px-8' : 'px-4'} w-screen mt-4 py-4 pb-8 drop-shadow-brand rounded-md text-right flex items-center justify-end gap-6 mobile-nav`}>
+              <DropdownMenu.Content className={`${backgroundColor} ${isDark ? 'px-4 md:px-8' : ''} w-screen mt-4 py-4 pb-8  drop-shadow-brand rounded-md text-right flex items-center justify-end gap-6 mobile-nav`}>
 
                   {activePage === 'home' ? (
                     <Link href="/" className='text-brand-primary hover:text-brand-soft' style={{transitionDuration: '.3s'}}>Home</Link>
@@ -62,9 +104,9 @@ export default function Header({ isDark, activePage }) {
                   )}
 
                   {activePage === 'about' ? (
-                    <Link href="/home" className='text-brand-primary hover:text-brand-soft' style={{transitionDuration: '.3s'}}>Home</Link>
+                    <Link href="/about" className='text-brand-primary hover:text-brand-soft' style={{transitionDuration: '.3s'}}>About</Link>
                   ) : (
-                    <Link href="/home" className={linkColor} style={{transitionDuration: '.3s'}}>About</Link>
+                    <Link href="/about" className={linkColor} style={{transitionDuration: '.3s'}}>About</Link>
                   )}
 
                   {/* {activePage === 'tryit' ? (
@@ -73,16 +115,16 @@ export default function Header({ isDark, activePage }) {
                     <Link href="/tryit" className={linkColor} style={{transitionDuration: '.3s'}}>Try It</Link>
                   )} */}
 
-                  {activePage === 'dashboard' ? (
+                  {/* {activePage === 'dashboard' ? (
                     <Link href="/dashboard" className='text-brand-primary hover:text-brand-soft' style={{transitionDuration: '.3s'}}>Dashboard</Link>
                   ) : (
                     <Link href="/dashboard" className={linkColor} style={{transitionDuration: '.3s'}}>Dashboard</Link>
                   )}
 
-                  <UserButton afterSignOutUrl='/'/>
-
+                  <UserButton afterSignOutUrl='/'/> */}
+{/* 
               </DropdownMenu.Content>
-          </DropdownMenu.Root>
+          </DropdownMenu.Root>  */}
         </div>
       </header>
     </>
